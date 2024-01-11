ad299f4e951c4eeed5e93556e195cc52691dbde3
unknown <arscaballerie@gmail.com>
2024-01-11 05:45:39 -0500

    Se agrego los primeros archivos de Funciones en Java

---
+++ Funciones/app.js
@@ -0,0 +1,34 @@
+// function saludar(){
+//     console.log('Hola a todos');
+// }
+
+// saludar();
+
+// function suma(numero1,numero2){
+//     return numero1+numero2;
+// }
+
+// console.log(suma(467,335));
+
+const saludar = () => console.log('Hola a todos');
+
+saludar();
+
+const sumar = (numero1,numero2) => numero1 + numero2;
+
+console.log(sumar(36, 25));
+
+/* CALLBACKS */
+
+const mostrartexto = (callback) => {
+    console.log('Mensaje 1');
+    callback;
+}
+
+const saludo = (nombre) => {
+    setTimeout(() => {
+        console.log(`Hola ${nombre}`)
+    }, 3000)
+}
+
+mostrartexto(saludo('Marco'));
---
+++ Funciones/exercise.js
@@ -0,0 +1,22 @@
+//Programar una funcion que solicite al usuario ingresar un texto ademas contar el numero de carateres del texto ingresado
+
+// let carater
+// const contarcaracter = (carater) => carater.length
+
+// console.log(contarcaracter("Jose"))
+
+const getString = () => {
+    //let mensaje = `la cadena ingresada tiene ${cadena.length} caracteres`
+    //console.log(mensaje);
+
+    let cadena = prompt('Ingrese un texto');
+    console.log(cadena);
+
+    if (cadena!='' && cadena!= null){
+        alert(`la cadena ingresada tiene ${cadena.length} caracteres`);
+    }else{
+        alert('Ingrese un texto por favor');
+    }
+}
+ 
+getString();
